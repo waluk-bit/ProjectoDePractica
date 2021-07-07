e892f0017895a79741595eede40a9d48c8608701
waluk-bit <lucaslopez167@hotmail.com>
2021-07-07 01:48:50 -0300

    Se crearon los archivos de migraciones

--- database/migrations/2021_07_06_185713_create_permiso_table.php
+++ database/migrations/2021_07_06_185713_create_permiso_table.php
@@ -4,7 +4,7 @@ use Illuminate\Support\Facades\Schema;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Database\Migrations\Migration;
 
-class CreateUsersTable extends Migration
+class CreatePermisoTable extends Migration
 {
     /**
      * Run the migrations.
@@ -13,12 +13,10 @@ class CreateUsersTable extends Migration
      */
     public function up()
     {
-        Schema::create('users', function (Blueprint $table) {
-            $table->bigIncrements('id');
-            $table->string('usuario', 50);
-            $table->string('password', 100);
-            $table->string('email', 50)->unique();
-            $table->rememberToken();
+        Schema::create('permiso', function (Blueprint $table) {
+            $table->increments('id');
+            $table->string('nombre', 50);
+            $table->string('slug', 50);
             $table->timestamps();
         });
     }
@@ -30,6 +28,6 @@ class CreateUsersTable extends Migration
      */
     public function down()
     {
-        Schema::dropIfExists('users');
+        Schema::dropIfExists('permiso');
     }
 }
